20e4bcac84ca82abf794e5b5979c0aa178c2aca5
Louis Taylor <kragniz@gmail.com>
2014-02-27 16:55:54 +0000

    hyper advanced straight line mk 2.1

--- simple.cpp
+++ simple.cpp
@@ -7,12 +7,82 @@
 using namespace PlayerCc;
 
 double getAngleDiff(double angle1, double angle2){
-  double a = angle2  - angle1;
-         a = fmod( (a + 180), 360 ) - 180;
-
-
-  return a;
+    double a = angle2  - angle1;
+    a = fmod( (a + 180), 360 ) - 180;
+    
+    return a;
 }
+
+double get_heading_between ( double x1, double y1, double x2, double y2 ){
+    double delta_x = x2 - x1;
+    double delta_y = y2 - y1;
+
+    double rads = atan( delta_x / delta_y);
+
+    return rtod(rads);
+
+}
+
+
+double distance_between( double x1, double y1, double x2, double y2 ){
+	double delta_x = x2 - x1;
+	double delta_y = y2 - y1;
+	return sqrt( pow( delta_x, 2 ) + pow( delta_y, 2));
+}
+
+class RobotMover{
+	private:
+		PlayerClient* robot;
+		Position2dProxy* position_proxy;
+
+		double p_coef, i_coef, d_coef;
+        double p, i, d;
+
+        double x_target, y_target;
+		
+        bool in_front( double x, double y ){
+            double world_heading = get_heading_between ( position_proxy->GetXPos(), position_proxy->GetYPos(), x_target, y_target );
+            double local_heading = getAngleDiff( position_proxy->GetYaw(), world_heading );
+            
+            if (local_heading > -90 || local_heading < 90){
+                return true;
+            } else { 
+                return false;
+            }
+        }
+
+
+	public:
+		RobotMover( double p_coef, double i_coef, double d_coef,
+                    PlayerClient* robot, Position2dProxy* position_proxy ){
+            this->p_coef = p_coef;
+            this->i_coef = i_coef;
+            this->d_coef = d_coef;
+            this->robot = robot;
+            this->position_proxy = position_proxy;
+		}
+		
+        void set_target( double x_target, double y_target ){
+            this->x_target = x_target;
+            this->y_target = y_target;
+        }
+
+
+        void move() {
+            
+            i = i + p;
+
+            robot->Read();
+
+            p = distance_between(position_proxy->GetXPos(), position_proxy->GetYPos(),
+                                 x_target, y_target);
+
+            if ( !in_front(x_target, y_target) ) p *= -1;
+
+            position_proxy->SetSpeed(0.1 * p + 0.1 * i, 0);  
+            std::cout << p << i << std::endl;
+        }
+};
 
 int turn_to( double targetAngle, PlayerClient* robot, Position2dProxy* pp){
   robot->Read();
@@ -90,15 +160,6 @@ int move_distance( double target_distance,
   return 0;
 }
 
-void go_to( double x, double y ){
-}
-
-double distance_between( double x1, y1, x2, y2 ){
-	double delta_x = x2 - x1;
-	double delta_y = y2 - y1;
-	return sqrt( pow( delta_x, 2 ) + pow( delta_y, 2);
-}
-
 void go_to( double x, double y, 
 	PlayerClient* robot, 
 	Position2dProxy* pp){
@@ -111,14 +172,12 @@ void go_to( double x, double y,
 
 		robot->Read();
 
-		p = distance_between( pp->GetXPos(), pp->GetYPos(), x, y)
+		p = distance_between( pp->GetXPos(), pp->GetYPos(), x, y);
 
 		pp->SetSpeed(0.1 * p + 0.1 * i, 0);  
 		std::cout << p << i << std::endl;
 		sleep(1); 
 	}
-
-	return 0;
 }
 
 
@@ -128,20 +187,19 @@ int main(int argc, char *argv[])
     using namespace PlayerCc;
 
     PlayerClient    robot("lisa.islnet");
-    SonarProxy      sp(&robot,0);
-    Position2dProxy pp(&robot,0);
+    SonarProxy      sp(&robot, 0);
+    Position2dProxy pp(&robot, 0);
+    
+    RobotMover* mover = new RobotMover( 0.1, 0.1, 0, &robot, &pp);
 
     pp.SetMotorEnable(true);
 
     double angle = 0;
-    //turn_to ( angle, &robot, &pp);
+    
+    mover->set_target( 0, 1 );
 
-    //angle = fmod(angle + 90, 360);
-    //turn_to( angle, &robot, &pp);
     while (true) {
-    	move_distance( 1, &robot, &pp);
-    	angle = fmod(angle + 90, 360);
-    	turn_to( angle, &robot, &pp);
+    	mover->move();
     }
 }
 
